de6ea5e2a8779bce5019148631ac9c51e89c70e9
CoresDev <coresemail@protonmail.com>
2018-09-14 08:39:48 +0300

    fix spork

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -144,11 +144,13 @@ void CMasternodeSync::GetNextAsset()
     case (MASTERNODE_SYNC_INITIAL):
     case (MASTERNODE_SYNC_FAILED): // should never be used here actually, use Reset() instead
         ClearFulfilledRequest();
-        RequestedMasternodeAssets = MASTERNODE_SYNC_SPORKS;
+        RequestedMasternodeAssets = MASTERNODE_SYNC_LIST;
         break;
+        /*
     case (MASTERNODE_SYNC_SPORKS):
         RequestedMasternodeAssets = MASTERNODE_SYNC_LIST;
         break;
+        */
     case (MASTERNODE_SYNC_LIST):
         RequestedMasternodeAssets = MASTERNODE_SYNC_MNW;
         break;
